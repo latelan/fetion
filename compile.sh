b8c90f17da3a30a780d2c804eb9102b143f725f4
abel <coolboy1353@163.com>
2014-03-03 23:32:44 +0800

    update complie.sh

--- compile.sh
+++ compile.sh
@@ -2,7 +2,5 @@
 
 # Complier the SendMsg.java
 
-javac ./src/Encrypt.java -d ./bin
-javac ./src/Encode.java -cp ./bin -d ./bin
 javac -Djava.ext.dirs=./lib ./src/SendMsg.java -cp ./bin -d ./bin
 echo compiler finished
